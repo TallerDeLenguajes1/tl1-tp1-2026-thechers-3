aee77cba1946ce27bff26997f198022e9f03f7b3
Matias <matiass.fm13@gmail.com>
2026-04-07 01:28:57 -0300

    Codigo misterioso resuelto

--- Punto_2/codigo_misterio.c
+++ Punto_2/codigo_misterio.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
-void f_alpha(int *p) {
-    int temp = *p;
-    int rev = 0;
-    while (temp > 0) {
-        rev = (rev * 10) + (temp % 10);
-        temp = temp / 10;
+void invertirNumero(int *p) {
+    int num = *p;
+    int aux = 0;
+    while (num > 0) {
+        aux = (aux * 10) + (num % 10);
+        num = num / 10;
     }
-    *p = rev;
+    *p = aux;
 }
 
-void f_beta(int *p) {
+void dividirInvertido(int *p) {
     *p = *p / 2;
 }
 
-void f_gamma(int *p) {
-    int temp = *p;
+void sumaDigitos(int *p) {
+    int num = *p;
     int suma = 0;
-    while (temp > 0) {
-        suma = suma + (temp % 10);
-        temp = temp / 10;
+    while (num > 0) {
+        suma = suma + (num % 10);
+        num = num / 10;
     }
     *p = *p + suma;
 }
 
-void procesar_enigma(int *valor_referencia) {
-    f_alpha(valor_referencia);
-    f_beta(valor_referencia);
-    f_gamma(valor_referencia);
+void procesar_datos(int *valor_referencia) {
+    invertirNumero(valor_referencia);
+    dividirInvertido(valor_referencia);
+    sumaDigitos(valor_referencia);
 }
 
 int main() {
-    int dato_secreto = 452;
+    int numero = 452;
     
-    printf("Iniciando depuracion con el valor: %d\n", dato_secreto);
+    printf("Iniciando depuracion con el valor: %d\n", numero);
     
     // Instrucción para el alumno: 
     // Pon un breakpoint aquí (F9) y usa F11 (Step Into) para entrar a cada función.
-    procesar_enigma(&dato_secreto);
+    procesar_datos(&numero);
     
-    printf("Resultado final del enigma: %d\n", dato_secreto);
+    printf("Resultado final del enigma: %d\n", numero);
     
     return 0;
 }
