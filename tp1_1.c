f6d68090eb58de7a6efc68ebd09397ec9fbd8223
Matias <matiass.fm13@gmail.com>
2026-04-07 02:09:51 -0300

    Ejercicio 4 terminado

--- tp1_1.c
+++ tp1_1.c
@@ -4,6 +4,19 @@ int main () {
 
        printf("Hola mundo\n");
 
+     int num;
+        int *puntNum;
+
+        puntNum = &num;
+    
+        num = 4;
+
+    printf("Contenido del puntero: %d\n", *puntNum);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", puntNum);
+    printf("Direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria de puntero: %p\n", &puntNum);
+    printf("Tamanio de memoria utilizado por la variable: %d\n", sizeof(num));
+    
     
     return 0;
 }
